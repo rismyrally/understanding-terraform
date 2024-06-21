3fde50063c012460fbcfed53f6d1de991e8c1084
Rismy Rally <rismyrally@gmail.com>
2024-06-21 21:10:22 +0530

    feat: add S3 backend and DynamoDB for terraform state management
    - configure S3 backend for terraform state storage with encryption and state locking
    - add `aws_s3_bucket` resource for storing terraform state
    - enable versioning and server-side encryption on the S3 bucket
    - add `aws_dynamodb_table` resource for state locking with `PAY_PER_REQUEST` billing mode
    - configure AWS provider region to `us-east-1`

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "understanding-terraform-tf-state"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,7 +20,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-08a0d1e16fc3f61ea" # Amazon Linux 2023 AMI
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "understanding-terraform-tf-state"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encrypt_conf" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
